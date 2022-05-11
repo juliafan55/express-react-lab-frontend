09f0862d24dcd3c679914db3abb2cec84d5daf56
juliafan55 <juliafan55@gmail.com>
2022-05-11 13:56:45 -0400

    frontend complete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
 
+//components
+import Header from './components/Header';
+import Footer from './components/Footer';
+import { Route, Routes } from "react-router-dom"
+
+//pages
+import Home from './pages/Home';
+import About from './pages/About';
+import Projects from './pages/Projects';
+
 function App() {
+
+  const URL = "http://localhost:4000/";
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Routes>
+        <Route exact path="/" >
+          <Projects URL={URL} />
+        </Route>
+        <Route path="/about">
+          <About URL={URL} />
+        </Route>
+      </Routes>
     </div>
   );
 }
